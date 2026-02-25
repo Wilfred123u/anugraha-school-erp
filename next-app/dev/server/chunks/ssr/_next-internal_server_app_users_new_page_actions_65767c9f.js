61abcead65928a6b8d50bb65cd65ea9d231ab1d2
Wilfred Naveen <naveendslv123@gmail.com>
2026-02-25 15:21:35 +0530

    Clean project remove build files

--- next-app/dev/server/chunks/ssr/_next-internal_server_app_users_new_page_actions_65767c9f.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/users/new/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_users_new_page_actions_65767c9f.js.map
--- next-app/dev/static/chunks/app_students_page_tsx_33dfb53b._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_xlsx_xlsx_mjs_fef98e92._.js",
-  "static/chunks/node_modules_next_dist_compiled_f576e975._.js",
-  "static/chunks/app_students_page_tsx_241e5cd4._.js"
-],
-    source: "dynamic"
-});
